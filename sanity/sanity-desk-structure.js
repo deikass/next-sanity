8ddb3eb1f21997d3cdcdf3dfb902f0da0772ae29
Deividas Kasperavičius <deividas.kasperavicius@nfq.com>
2024-02-06 11:43:14 +0200

    Added Filtered Reviews to sanity Desktop

--- sanity/sanity-desk-structure.js
+++ sanity/sanity-desk-structure.js
@@ -16,7 +16,7 @@ export const myStructure = (S) => {
                 .child(S.document().schemaType('colors').documentId('colors'))
             ])
         ),
-        S.listItem()
+      S.listItem()
         .title('Modules')
         .child(
           S.list()
@@ -30,8 +30,78 @@ export const myStructure = (S) => {
                 .child(S.document().schemaType('footerModule').documentId('footerModule'))
             ])
         ),
+      S.divider(),
+      S.listItem()
+        .title('Filtered Reviews')
+        .child(
+          S.list()
+            .title('Filters')
+            .items([
+              S.listItem()
+                .title('Reviews By Rating')
+                .child(
+                  S.list()
+                    .title('Reviews By Rating')
+                    .items([
+                      S.listItem()
+                        .title('1 - Star *')
+                        .child(
+                          S.documentList()
+                            .title('1 - Star Reviews')
+                            .filter('_type == "review" && rating == "1"')
+                        ),
+                      S.listItem()
+                        .title('2 - Stars **')
+                        .child(
+                          S.documentList()
+                            .title('2 - Stars Reviews')
+                            .filter('_type == "review" && rating == "2"')
+                        ),
+                      S.listItem()
+                        .title('3 - Stars ***')
+                        .child(
+                          S.documentList()
+                            .title('3 - Stars Reviews')
+                            .filter('_type == "review" && rating == "3"')
+                        ),
+                      S.listItem()
+                        .title('4 - Stars ****')
+                        .child(
+                          S.documentList()
+                            .title('4 - Stars Reviews')
+                            .filter('_type == "review" && rating == "4"')
+                        ),
+                      S.listItem()
+                        .title('5 - Stars *****')
+                        .child(
+                          S.documentList()
+                            .title('5 - Stars Reviews')
+                            .filter('_type == "review" && rating == "5"')
+                        ),
+                    ])
+                ),
+              S.listItem()
+                .title('Reviews By User')
+                .child(
+                  S.documentTypeList('user')
+                    .title('Reviews by user')
+                    .child((userId) =>
+                      S.documentList()
+                        .title('Reviews')
+                        .filter('_type == "review" && $userId == user._ref')
+                        .params({ userId })
+                    )
+                ),
+            ])
+        ),
+      S.listItem().title('All reviews').child(
+        S.documentList()
+          .title('All Reviews')
+          .filter('_type == "review"')
+      ),
+      S.divider(),
       ...S.documentTypeListItems().filter(
         (listItem) => !['siteSettings', 'colors', 'headerModule', 'footerModule'].includes(listItem.getId())
-        ),
+      ),
     ]);
 };
